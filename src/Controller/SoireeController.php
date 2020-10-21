250279b61ee01dd3892725320aa4cedf36140b2e
Kraystone <leo.catifait@epsi.fr>
2020-10-21 11:33:35 +0200

    add controller SoireeController.php add ajouter.html.twig modif css modif navbar modif

--- src/Controller/SoireeController.php
+++ src/Controller/SoireeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Soiree;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,5 +17,24 @@ class SoireeController extends AbstractController
         return $this->render('soiree/index.html.twig', [
             'controller_name' => 'SoireeController',
         ]);
+    }
+
+    /**
+     * @Route("/soiree/ajouter", name="soiree_ajouter")
+     */
+    public function ajouter(Request $request)
+    {
+        $soiree = new Soiree();
+        $form = $this->createForm(Soiree::class, $soiree);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($soiree);
+            $em->flush();
+            return $this->redirectToRoute("soiree");
+        }
+        return $this->render("soiree/ajouter.html.twig", [
+                "formulaire" => $form->createView()
+            ]);
     }
 }
