cca1b9138be015fd19ba363683332cf3ea4c4a60
Marion-Bariteau <marion.bariteau@epsi.fr>
2020-11-08 10:13:23 +0100

    form TableauSoirée Créer

--- src/Controller/SoireeController.php
+++ src/Controller/SoireeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+
 use App\Entity\Soiree;
+use App\Form\SoireeSupprimerType;
 use App\Form\SoireeType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,12 +13,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class SoireeController extends AbstractController
 {
     /**
-     * @Route("/soiree", name="soiree")
+     * @Route("/soiree/index/{id}", name="soiree_index")
      */
-    public function index()
+    public function index($id)
     {
+        $repo=$this->getDoctrine()->getRepository(Soiree::class);
+        $soiree= $repo->find($id);
+
         return $this->render('soiree/index.html.twig', [
-            'controller_name' => 'SoireeController',
+            'soiree' => $soiree,
         ]);
     }
 
@@ -32,10 +37,46 @@ class SoireeController extends AbstractController
             $em = $this->getDoctrine()->getManager();
             $em->persist($soiree);
             $em->flush();
-            return $this->redirectToRoute("soiree");
+            return $this->redirectToRoute("home");
         }
         return $this->render("soiree/ajouter.html.twig", [
                 "formulaire" => $form->createView()
             ]);
     }
+
+    /**
+     * @Route("/soiree/supprimer/{id}", name="soiree_supprimer")
+     */
+
+    public function supprimer($id, Request $request){
+        $repo=$this->getDoctrine()->getRepository(Soiree::class);
+        $soireesup= $repo->find($id);
+
+        //création du formulaire
+        $form=$this->createForm(SoireeSupprimerType::class, $soireesup);
+
+
+        //recup du POST
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //recup de l'entitymanager -> gere les objet
+            $em=$this->getDoctrine()->getManager();
+
+            //dire au manager qu'on veut garder notre objet en BDD
+            $em->remove($soireesup);
+
+            //générer l'insert
+            $em->flush();
+
+            //aller a la liste des catégories
+            return $this->redirectToRoute("soiree_supprimer");
+
+        }
+
+        return $this->render("soiree/supprimer.html.twig",[
+            "formulaire" => $form->createView(),
+            "soiree"=>$soireesup,
+        ]);
+    }
+
 }
