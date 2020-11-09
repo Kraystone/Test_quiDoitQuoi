9479cf6f0da0e8cd6ba78379aab17af29534ba3b
Marion-Bariteau <marion.bariteau@epsi.fr>
2020-11-09 23:57:29 +0100

    tout refait manque tablo + bouton supprime

--- src/Controller/SoireeController.php
+++ src/Controller/SoireeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Soiree;
 use App\Form\SoireeType;
+use App\Form\SupprimerSoireeType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class SoireeController extends AbstractController
 {
     /**
-     * @Route("/soiree", name="soiree")
+     * @Route("/soiree/index/{id}", name="soiree_index")
      */
-    public function index()
+    public function index($id, Request $request)
     {
+        $repo=$this->getDoctrine()->getRepository(Soiree::class);
+        $soiree= $repo->find($id);
+
         return $this->render('soiree/index.html.twig', [
-            'controller_name' => 'SoireeController',
+            'soiree' => $soiree,
         ]);
     }
 
@@ -40,38 +44,38 @@ class SoireeController extends AbstractController
         ]);
     }
 
-//    /**
-//     * @Route("/soiree/supprimer/{id}", name="soiree_supprimer")
-//     */
-//
-//    public function supprimer($id, Request $request){
-//        $repo=$this->getDoctrine()->getRepository(Soiree::class);
-//        $soireesup= $repo->find($id);
-//
-//        //création du formulaire
-//        $form=$this->createForm(SoireeSupprimerType::class, $soireesup);
-//
-//
-//        //recup du POST
-//        $form->handleRequest($request);
-//        if($form->isSubmitted() && $form->isValid()){
-//            //recup de l'entitymanager -> gere les objet
-//            $em=$this->getDoctrine()->getManager();
-//
-//            //dire au manager qu'on veut garder notre objet en BDD
-//            $em->remove($soireesup);
-//
-//            //générer l'insert
-//            $em->flush();
-//
-//            //aller a la liste des catégories
-//            return $this->redirectToRoute("soiree_supprimer");
-//
-//        }
-//
-//        return $this->render("soiree/supprimer.html.twig",[
-//            "formulaire" => $form->createView(),
-//            "soiree"=>$soireesup,
-//        ]);
-//    }
+    /**
+     * @Route("/soiree/supprimer/{id}", name="soiree_supprimer")
+     */
+
+    public function supprimer($id, Request $request){
+        $repo=$this->getDoctrine()->getRepository(Soiree::class);
+        $soireesup= $repo->find($id);
+
+        //création du formulaire
+        $form=$this->createForm(SupprimerSoireeType::class, $soireesup);
+
+
+        //recup du POST
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //recup de l'entitymanager -> gere les objet
+            $em=$this->getDoctrine()->getManager();
+
+            //dire au manager qu'on veut garder notre objet en BDD
+            $em->remove($soireesup);
+
+            //générer l'insert
+            $em->flush();
+
+            //aller a la liste des catégories
+            return $this->redirectToRoute("soiree_supprimer");
+
+        }
+
+        return $this->render("soiree/supprimer.html.twig",[
+            "formulaire" => $form->createView(),
+            "soiree"=>$soireesup,
+        ]);
+    }
 }
